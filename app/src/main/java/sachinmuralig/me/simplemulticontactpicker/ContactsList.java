d6b3d02f8465e4f0c46587e144cfa805eb1823f2
Sachin Murali . G <sachin.m@ifmr.co.in>
2016-04-10 20:36:01 +0530

    alpha version

---
+++ app/src/main/java/sachinmuralig/me/simplemulticontactpicker/ContactsList.java
@@ -0,0 +1,48 @@
+package sachinmuralig.me.simplemulticontactpicker;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sachin.M on 02-04-2016.
+ */
+public class ContactsList{
+
+    public ArrayList<Contact> contactArrayList;
+
+    ContactsList(){
+
+        contactArrayList = new ArrayList<Contact>();
+    }
+
+
+
+    public int getCount(){
+
+        return contactArrayList.size();
+    }
+
+    public void addContact(Contact contact){
+        contactArrayList.add(contact);
+    }
+
+    public  void removeContact(Contact contact){
+        contactArrayList.remove(contact);
+    }
+
+    public Contact getContact(int id){
+
+        for(int i=0;i<this.getCount();i++){
+            if(Integer.parseInt(contactArrayList.get(i).id)==id)
+                return contactArrayList.get(i);
+        }
+
+        return null;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/sachinmuralig/me/simplemulticontactpicker/MainActivity.java
@@ -0,0 +1,64 @@
+package sachinmuralig.me.simplemulticontactpicker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView contactsDisplay;
+    Button pickContacts;
+    final int CONTACT_PICK_REQUEST = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        contactsDisplay = (TextView) findViewById(R.id.txt_selected_contacts);
+        pickContacts = (Button) findViewById(R.id.btn_pick_contacts);
+
+        pickContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentContactPick = new Intent(MainActivity.this,ContactsPickerActivity.class);
+                MainActivity.this.startActivityForResult(intentContactPick,CONTACT_PICK_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CONTACT_PICK_REQUEST && resultCode == RESULT_OK){
+
+            ArrayList<Contact> selectedContacts = data.getParcelableArrayListExtra("SelectedContacts");
+
+            String display="";
+            for(int i=0;i<selectedContacts.size();i++){
+
+                display += (i+1)+". "+selectedContacts.get(i).toString()+"\n";
+
+            }
+            contactsDisplay.setText("Selected Contacts : \n\n"+display);
+
+        }
+
+    }
+
+
+}
